96f85d165be156dfe1ec0a84ad23a4dac299922b
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-04-26 19:17:20 -0300

    adicionado Switch e nova Route para a pagina do carrinho de compras no App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import * as api from './services/api';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default class App extends React.Component {
   async componentDidMount() {
@@ -13,7 +14,10 @@ export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ Cart } />
+        </Switch>
       </BrowserRouter>
     );
   }
