4d07397e377340ebe0442e1e9586da2dada8057d
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2022-04-26 16:05:26 -0300

    feat: implementa funcao de requisicao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
-// import { getCategories } from './services/api';
 import * as api from './services/api';
-/* commnet */
-export default class App extends React.Component {
-  componentDidMount() {
-    api.getCategories('construcao').then((categories) => { console.log(categories); });
-  }
 
-  /*  showApi = async () => {
-    console.log(await getCategories('computador'));
-  } */
+export default class App extends React.Component {
+  async componentDidMount() {
+    api.getCategories();
+    // const result = await api.getProductsFromCategoryAndQuery('MLB5672', 'Acessórios para Veículos');
+  }
 
   render() {
     return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-export async function getCategories(id) {
+export async function getCategories() {
   // Implemente aqui
   try {
-    const endpoint = `https://api.mercadolibre.com/sites/MLB/categories=${id}`;
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
     const response = await fetch(endpoint);
     const data = await response.json();
     return data;
@@ -10,6 +10,14 @@ export async function getCategories(id) {
   }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  try {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+  } catch (erro) {
+    return erro;
+  }
 }
