e44c9f563ce7d5fff76e217e2f8d01532a2122e7
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2022-04-26 15:44:58 -0300

    resquita api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-/*commnet*/
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { render } from '@testing-library/react';
+// import { getCategories } from './services/api';
+import * as api from './services/api';
+/* commnet */
+export default class App extends React.Component {
+  componentDidMount() {
+    api.getCategories('construcao').then((categories) => { console.log(categories); });
+  }
 
-export default App;
+  /*  showApi = async () => {
+    console.log(await getCategories('computador'));
+  } */
+
+  render() {
+    return (
+      <div>
+        <p>Teste</p>
+      </div>
+    );
+  }
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,13 @@
-export async function getCategories() {
+export async function getCategories(id) {
   // Implemente aqui
+  try {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/categories=${id}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+  } catch (erro) {
+    return erro;
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
