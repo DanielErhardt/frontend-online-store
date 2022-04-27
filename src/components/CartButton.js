2eff75a95e7e7bbf8e20ac217493720a8d2e7c67
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-04-27 13:52:42 -0300

    adicionado componente visual ao botão carrinho de compras

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default class CartButton extends React.Component {
   render() {
-    return (<Link to="/cart" data-testid="shopping-cart-button" />);
+    return (<Link to="/cart" data-testid="shopping-cart-button">Carrinho de Compras</Link>);
   }
 }
