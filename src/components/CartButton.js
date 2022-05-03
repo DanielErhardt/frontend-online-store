77430533a1fbe732e368697bf87190bbc68b6be9
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-05-03 14:51:52 -0300

    adiciona o ícone do carrinho de compras e cria um arquivo para o CSS do CartButton

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cartLogo from '../Images/cartLogo.png';
+import './CartButton.css';
 
 export default class CartButton extends React.Component {
   render() {
     return (
       <Link to="/cart" data-testid="shopping-cart-button">
-        Carrinho de Compras
+        {/* <h3>Ir para o Carrinho</h3> */}
+        <img className="cartLogo" src={ cartLogo } alt="Carrinho" />
       </Link>
     );
   }
